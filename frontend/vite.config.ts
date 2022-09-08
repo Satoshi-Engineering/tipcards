c3438c93dad8eae36b68a65b6f7b832f725ed99d
David Scheuch <dave@satoshiengineering.com>
2022-09-08 12:58:14 +0200

    [TASK] Add some options to optimize l18n build size projects#46

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
 })
