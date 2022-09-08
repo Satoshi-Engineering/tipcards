69cdd11f3fd416103bc8c420afa90ab3ae5a29ec
David Scheuch <dave@satoshiengineering.com>
2022-09-08 11:48:25 +0200

    [TASK] Add lnurlHelpers.ts module projects#172

---
+++ frontend/src/modules/lnurlHelpers.ts
@@ -0,0 +1,18 @@
+import { Buffer } from 'buffer'
+import { bech32 } from 'bech32'
+
+export const encodeLnurl = (unencoded: string) => {
+  const words = bech32.toWords(Buffer.from(unencoded, 'utf8'))
+  const rules = {
+    prefix: 'lnurl',
+    limit: 1023,
+  }
+	return bech32.encode(rules.prefix, words, rules.limit)
+}
+
+export const decodeLnurl = (encoded: string) => {
+  const { words: dataPart } = bech32.decode(encoded, 2000)
+  const requestByteArray = bech32.fromWords(dataPart)
+
+  return Buffer.from(requestByteArray).toString()
+}
