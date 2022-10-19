a561cbd3ae4314e5e221414f39e95b16d6b20a33
Thomas Schagerl <thomas.schagerl@gmx.net>
2022-10-19 10:32:11 +0200

    [REFACTOR] remove hardcoded sate email projects#218

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <RouterView />
-    <footer class="mx-auto mt-auto pt-20 px-2 pb-2 w-full max-w-md print:hidden">
+    <footer
+      v-if="SUPPORT_EMAIL != null"
+      class="mx-auto mt-auto pt-20 px-2 pb-2 w-full max-w-md print:hidden"
+    >
       <small class="block text-gray-400">
         <Translation keypath="footer.support">
           <template #email>
-            <LinkDefault href="mailto:support@sate.tools?subject=Lightning%20Tip%20Cards%20Feedback">support@sate.tools</LinkDefault>
+            <LinkDefault :href="`mailto:${SUPPORT_EMAIL}?subject=Lightning%20Tip%20Cards%20Feedback`">{{ SUPPORT_EMAIL }}</LinkDefault>
           </template>
         </Translation>
       </small>
@@ -16,5 +19,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { Translation } from 'vue-i18n'
-import LinkDefault from './components/typography/LinkDefault.vue'
+
+import LinkDefault from '@/components/typography/LinkDefault.vue'
+import { SUPPORT_EMAIL } from '@/constants'
 </script>
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,1 +1,2 @@
 export const BACKEND_API_ORIGIN = import.meta.env.VITE_BACKEND_API_ORIGIN || document.location.origin
+export const SUPPORT_EMAIL = import.meta.env.VITE_SUPPORT_EMAIL || null
