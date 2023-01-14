3809deb73f7f5e89e114b2b5610d7f3b5bd80cd7
dr-erych <dave@satoshiengineering.com>
2023-01-14 09:38:56 +0100

    [TASK] Add stubs for about and terms pages projects#244

---
+++ frontend/src/pages/PageTerms.vue
@@ -0,0 +1,16 @@
+
+import HeadlineDefault from '@/components/typography/HeadlineDefault.vue';
+
+<template>
+  <div class="mx-auto w-full max-w-md px-4">
+    <BackLink class="pt-4" />
+    <HeadlineDefault level="h1">
+      Terms of service
+    </HeadlineDefault>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeadlineDefault from '@/components/typography/HeadlineDefault.vue'
+import BackLink from '@/components/BackLink.vue'
+</script>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,6 +7,8 @@ const PageLanding = () => import('@/pages/PageLanding.vue')
 const PageCards = () => import('@/pages/PageCards.vue')
 const PageFunding = () => import('@/pages/PageFunding.vue')
 const PageAuthDevelopment = () => import('@/pages/PageAuthDevelopment.vue')
+const PageAbout = () => import('@/pages/PageAbout.vue')
+const PageTerms = () => import('@/pages/PageTerms.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -43,6 +45,18 @@ const router = createRouter({
       name: 'funding',
       component: PageFunding,
       meta: { title: () => i18n.global.t('funding.title') },
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: PageAbout,
+      meta: { title: () => 'About' },
+    },
+    {
+      path: '/terms',
+      name: 'terms',
+      component: PageTerms,
+      meta: { title: () => 'Terms of service' },
     },
     {
       path: '/auth',
