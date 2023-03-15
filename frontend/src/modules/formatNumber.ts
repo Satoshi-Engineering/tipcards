ada2b257023dbc2007b83fa261c4f22d52341c23
dr-erych <dave@satoshiengineering.com>
2023-03-15 17:24:37 +0100

    [BUGFIX] Re-add locale option to formatNumber projects#428

--- frontend/src/modules/formatNumber.ts
+++ frontend/src/modules/formatNumber.ts
@@ -1,8 +1,11 @@
 import i18n from '@/modules/initI18n'
 
-export default (value: number, maximumFractionDigits: number, minimumFractionDigits = 0, roundUp = false): string => {
+export default (value: number, maximumFractionDigits: number, minimumFractionDigits = 0, roundUp = false, locale: string | undefined = undefined): string => {
   if (roundUp && countDecimals(value) > maximumFractionDigits) {
     value = Math.ceil(value * Math.pow(10, maximumFractionDigits)) / Math.pow(10, maximumFractionDigits)
+  }
+  if (locale != null) {
+    return value.toLocaleString(locale, { maximumFractionDigits, minimumFractionDigits })    
   }
   return i18n.global.n(value, { maximumFractionDigits, minimumFractionDigits })
 }
