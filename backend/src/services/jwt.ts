7888c510db7c33593a1462a611e8889f087b4645
Thomas Schagerl <thomas.schagerl@gmx.net>
2023-04-17 16:35:59 +0200

    [BUGFIX] typecheck projects#329

--- backend/src/services/jwt.ts
+++ backend/src/services/jwt.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express'
+import type { Request, Response, NextFunction } from 'express'
 import fs from 'fs'
 import {
   generateKeyPair, type KeyLike, SignJWT, jwtVerify,
