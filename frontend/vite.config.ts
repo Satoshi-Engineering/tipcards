eeaf7ddbdf9874a945be3ff846748a40687d46b5
Thomas Schagerl <thomas.schagerl@gmx.net>
2023-05-05 09:20:06 +0200

    [TASK] add env option to build maintenance page projects#494

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
+import { resolve } from 'path'
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createHtmlPlugin } from 'vite-plugin-html'
@@ -7,6 +7,16 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+
+  let input: Record<string, string> = {
+    'index': resolve(__dirname, 'index.html'),
+  }
+  if (env.BUILD_MAINTENANCE) {
+    input = {
+      'maintenance': resolve(__dirname, 'index.maintenance.html'),
+    }
+  }
+
   return {
     plugins: [vue(), createHtmlPlugin({ inject: { data: { env } } })],
     resolve: {
@@ -20,6 +30,11 @@ export default defineConfig(({ mode }) => {
       __VUE_I18N_FULL_INSTALL__: true,
       __VUE_I18N_LEGACY_API__: false,
       __INTLIFY_PROD_DEVTOOLS__: false,
+    },
+    build: {
+      rollupOptions: {
+        input,
+      },
     },
   }
 })
