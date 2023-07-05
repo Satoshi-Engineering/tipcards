9b34721f8711ba49c4c82c73b62139468e516ad4
Thomas Schagerl <tom@satoshiengineering.com>
2023-07-05 15:42:49 +0200

    [FEATURE] ModalLogin show email hint if user has no email address in profile projects#559

--- frontend/src/components/ModalLogin.vue
+++ frontend/src/components/ModalLogin.vue
@@ -16,6 +16,13 @@
       :error="error ? $t('auth.modalLogin.loginErrorText') : undefined"
       :success="isLoggedIn"
     />
+    <p v-if="isLoggedIn && missingEmail" class="mt-12 text-sm">
+      {{ $t('auth.modalLogin.emailHint') }}
+      <br>
+      <LinkDefault :to="{ name: 'user-account' }">
+        {{ $t('auth.modalLogin.emailCta') }}
+      </LinkDefault>
+    </p>
     <div class="mt-12 text-center">
       <ButtonDefault
         class="text-sm min-w-[170px]"
@@ -38,6 +45,7 @@ import ModalDefault from '@/components/ModalDefault.vue'
 import AnimatedLoadingWheel from '@/components/AnimatedLoadingWheel.vue'
 import LightningQrCode from '@/components/LightningQrCode.vue'
 import ButtonDefault from '@/components/ButtonDefault.vue'
+import LinkDefault from '@/components/typography/LinkDefault.vue'
 import { useUserStore } from '@/stores/user'
 import { BACKEND_API_ORIGIN } from '@/constants'
 
@@ -50,6 +58,7 @@ const fetchingLogin = ref(true)
 const lnurl = ref<string>()
 const hash = ref<string>()
 const error = ref(false)
+const missingEmail = ref(false)
 let socket: Socket
 
 onBeforeMount(async () => {
@@ -75,7 +84,15 @@ const connectSocket = () => {
     if (hash.value == null) {
       return
     }
-    login(hash.value)
+    await login(hash.value)
+    try {
+      const { data } = await axios.get(`${BACKEND_API_ORIGIN}/api/auth/profile`)
+      if (typeof data.data?.email !== 'string' || data.data.email.length === 0) {
+        missingEmail.value = true
+      }
+    } catch (error) {
+      console.error(error)
+    }
   })
   socket.on('connect', () => {
     socket.emit('waitForLogin', { hash: hash.value })
