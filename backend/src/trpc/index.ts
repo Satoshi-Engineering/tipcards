4b6626fbdffe93b9fac822c9e2978c3587b2a4a3
Thomas Schagerl <tom@satoshiengineering.com>
2023-10-11 13:16:40 +0200

    [REFACTOR] move trpc custom error handling from router to global onError func projects#665

--- backend/src/trpc/index.ts
+++ backend/src/trpc/index.ts
@@ -1,5 +1,9 @@
 import type { OnErrorFunction } from '@trpc/server/dist/internals/types'
+import { TRPCError } from '@trpc/server'
 import type { Request } from 'express'
+import { ZodError } from 'zod'
+
+import { ErrorWithCode } from '../../../src/data/Errors'
 
 import { router, createContext } from './trpc'
 import { cardRouter } from './router/card'
@@ -13,6 +17,20 @@ export const appRouter = router({
 export type AppRouter = typeof appRouter
 
 export const onError: OnErrorFunction<AppRouter, Request> = (opts) => {
+  if (opts.error.cause instanceof ErrorWithCode) {
+    opts.error = new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: `Error with code: ${opts.error.cause.code}`,
+      cause: opts.error.cause.error,
+    })
+  } else if (opts.error.cause instanceof ZodError) {
+    opts.error = new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Unexpected zod parsing error.',
+      cause: opts.error.cause,
+    })
+  }
+
   if (opts.error.code === 'INTERNAL_SERVER_ERROR') {
     console.error(opts)
   }
--- backend/src/trpc/router/set.ts
+++ backend/src/trpc/router/set.ts
@@ -1,8 +1,3 @@
-import { TRPCError } from '@trpc/server'
-import { ZodError } from 'zod'
-
-import { ErrorWithCode } from '../../../../src/data/Errors'
-
 import CardCollectionRedis from '../../modules/CardCollectionRedis'
 import { getSetsByUserId } from '../../services/database'
 
@@ -12,48 +7,19 @@ import { Set } from '../data/Set'
 import { router, publicProcedure } from '../trpc'
 import { loggedInProcedure } from '../loggedInProcedure'
 
-// todo : check if I can use middleware for try/catch and "generic" error handling
 export const setRouter = router({
   getAll: loggedInProcedure
     .output(Set.array())
     .query(async ({ ctx }) => {
-      try {
-        const setsDatabase = await getSetsByUserId(ctx.accessToken.id)
-        return setsDatabase.map((set) => SetFromSetDatabase.parse(set))
-      } catch (error) {
-        let message = 'Unexpected database error.'
-        if (error instanceof ZodError) {
-          message = 'Unexpected zod parsing error.'
-        }
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message,
-          cause: error,
-        })
-      }
+      const setsDatabase = await getSetsByUserId(ctx.accessToken.id)
+      return setsDatabase.map((set) => SetFromSetDatabase.parse(set))
     }),
 
   getCards: publicProcedure
     .input(Set.shape.id)
     .output(Card.array())
     .query(async ({ input: setId }) => {
-      try {
-        const cards = await CardCollectionRedis.fromSetId(setId)
-        return await cards.toTRpcResponse()
-      } catch (error) {
-        let message = 'Unexpected database error.'
-        let cause = error
-        if (error instanceof ErrorWithCode) {
-          message = `Error with code: ${error.code}`
-          cause = error.error
-        } if (error instanceof ZodError) {
-          message = 'Unexpected zod parsing error.'
-        }
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message,
-          cause,
-        })
-      }
+      const cards = await CardCollectionRedis.fromSetId(setId)
+      return await cards.toTRpcResponse()
     }),
 })
