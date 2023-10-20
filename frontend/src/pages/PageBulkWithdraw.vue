8a960e8834f7b8d66ee9ff78bfa6573928f42b47
Thomas Schagerl <tom@satoshiengineering.com>
2023-10-20 10:23:47 +0200

    [FEATURE] PageBulkWithdraw show info if a card is already locked projects#706

--- frontend/src/pages/PageBulkWithdraw.vue
+++ frontend/src/pages/PageBulkWithdraw.vue
@@ -1,51 +1,45 @@
 <template>
   <div class="mt-3 mx-auto w-full max-w-md px-4">
-    <ul class="mb-5">
-      <li
-        v-for="set in sets"
-        :key="set.id"
-        class="mb-2"
-      >
-        <h5 class="font-bold">
-          Set {{ set.name }}
-        </h5>
-        <small class="block">({{ set.id }})</small>
-        <p v-if="cardsBySet[set.id] == null || cardsBySet[set.id].length === 0">
-          No cards in the set.
-        </p>
-        <template v-else>
-          <h5>
-            Cards:
-          </h5>
-          <ul>
-            <li v-for="card in cardsBySet[set.id]" :key="card.hash">
-              {{ card.hash }}
-            </li>
-          </ul>
+    <HeadlineDefault
+      level="h1"
+      class="mt-10"
+    >
+      {{ $t('bulkWithdraw.headline') }}
+    </HeadlineDefault>
+    <p class="my-3">
+      <Translation keypath="bulkWithdraw.setName">
+        <template #setName>
+          <strong>{{ settings.setName || $t('index.unnamedSetNameFallback') }}</strong>
         </template>
-      </li>
-    </ul>
-
-    <ButtonDefault variant="outline" @click="safeReload">
-      reload
-    </ButtonDefault>
-    <ButtonDefault @click="withdraw">
-      withdraw
-    </ButtonDefault>
+      </Translation>
+    </p>
+    <div v-if="cardLockedByWithdraw != null">
+      <p class="mb-4">
+        {{ $t('bulkWithdraw.withdrawExists') }}
+      </p>
+      <ButtonWithTooltip
+        type="submit"
+        variant="outline"
+        @click="resetBulkWithdraw"
+      >
+        {{ $t('bulkWithdraw.buttonReset') }} 
+      </ButtonWithTooltip>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { computed, onBeforeMount, ref, reactive } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import type z from 'zod'
+
 
 import type { Settings } from '@shared/data/redis/Set'
 
 import type { Card } from '@backend/trpc/data/Card'
-import type { Set } from '@backend/trpc/data/Set'
 
-import ButtonDefault from '@/components/ButtonDefault.vue'
+import HeadlineDefault from '@/components/typography/HeadlineDefault.vue'
+import ButtonWithTooltip from '@/components/ButtonWithTooltip.vue'
+import Translation from '@/modules/I18nT'
 import hashSha256 from '@/modules/hashSha256'
 import useTRpc from '@/modules/useTRpc'
 import {
@@ -86,48 +80,19 @@ const loadCards = async () => {
   ))
 }
 
-// todo : if there is an active bulk withdraw for a card show info to user (with a reset button)
+const cardLockedByWithdraw = computed(() => cards.value?.find((card) => card.isLockedByBulkWithdraw))
+
+const resetBulkWithdraw = async () => {
+  if (cardLockedByWithdraw.value == null) {
+    return
+  }
+  await client.bulkWithdraw.deleteByCardHash.mutate(cardLockedByWithdraw.value.hash)
+}
+
 // todo : if there are no funded cards show info to user (with a back button)
 // todo : show the list of cards that are funded (like status list on PageCards) + button for withdraw creation
 // todo : on withdraw creaton show withdraw link
 // todo : handle loading states
 // todo : handle errors
-
-type SetId = z.infer<typeof Set.shape.id>
-
-const sets = ref<Set[]>([])
-const cardsBySet = ref<Record<SetId, Card[]>>({})
-
-const reload = async () => {
-  sets.value = await client.set.getAll.query()
-
-  cardsBySet.value = (await Promise.all(sets.value.map(async (set) => {
-    const cards = await client.set.getCards.query(set.id)
-    return { set, cards }
-  }))).reduce((total, { set, cards }) => ({
-    ...total,
-    [set.id]: cards,
-  }), {} as Record<SetId, Card[]>)
-}
-
-const safeReload = async () => {
-  try {
-    await reload()
-  } catch (error) {
-    // todo : add error handling (i.e. show error to user)
-    console.error(error)
-  }
-}
-
-onBeforeMount(safeReload)
-
-type CardHash = z.infer<typeof Card.shape.hash>
-const withdraw = async () => {
-  client.bulkWithdraw.createForCards.mutate(
-    Object.values(cardsBySet.value).reduce((cardHashes, cards) => [
-      ...cardHashes,
-      ...cards.map((card) => card.hash),
-    ], [] as CardHash[]),
-  )
-}
+// todo : clean up file (move stuff into sub-components or composables)
 </script>
