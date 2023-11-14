9b2d320052f952b90a225d180868fecf2db66dac
thespielplatz <informatics@gmx.net>
2023-11-14 11:44:37 +0100

    [TASK] update from dbml projects#750

--- backend/src/database/drizzle/schema/LnurlP.ts
+++ backend/src/database/drizzle/schema/LnurlP.ts
@@ -4,6 +4,7 @@ export const LnurlP = mysqlTable('LnurlP', {
   lnbitsId: varchar('lnbitsId', { length: 36 }).primaryKey().unique().notNull(), // Note: id from lnbits lnurlp link
   created: datetime('created').notNull(),
   expiresAt: datetime('expiresAt'),
+  finished: datetime('finished'), // Note: for shared funding the user has to manually trigger the finishing, for single funding this is set as soon as the first payment is registered
 })
 
 export type LnurlP = typeof LnurlP.$inferSelect
