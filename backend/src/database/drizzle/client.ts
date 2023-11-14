47728cfe71ce078e94950a496100d1c4fbd25b8f
Thomas Schagerl <tom@satoshiengineering.com>
2023-11-14 16:59:15 +0100

    [SPIKE] add drizzle integration tests WIP projects#751

--- backend/src/database/drizzle/client.ts
+++ backend/src/database/drizzle/client.ts
@@ -1,12 +1,18 @@
 import { drizzle } from 'drizzle-orm/mysql2'
 import mysql from 'mysql2/promise'
 
+import * as schema from './schema'
+const MYSQL_PORT = parseInt(process.env.MYSQL_PORT || '3306')
+
 export const getClient = async () => {
   const connection = await mysql.createConnection({
-    host: 'host',
-    user: 'user',
-    database: 'database',
-  })
+    host: process.env.MYSQL_HOST,
+    port: MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    multipleStatements: true,
+ })
    
-  return drizzle(connection)
+  return drizzle(connection, { mode: 'default', schema })
 }
