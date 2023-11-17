c55749e54a69b7636860246907b145b0d25d09a3
thespielplatz <informatics@gmx.net>
2023-11-17 10:53:13 +0100

    [TASK] add pk to CardVersionHasInvoice projects#751

--- backend/src/database/drizzle/schema/CardVersionHasInvoice.ts
+++ backend/src/database/drizzle/schema/CardVersionHasInvoice.ts
@@ -1,10 +1,14 @@
-import { mysqlTable, varchar } from 'drizzle-orm/mysql-core'
+import { mysqlTable, primaryKey, varchar } from 'drizzle-orm/mysql-core'
 import { CardVersion } from './CardVersion'
 import { Invoice } from './Invoice'
 
 export const CardVersionHasInvoice = mysqlTable('CardVersionHasInvoice', {
   cardVersion: varchar('cardVersion', { length: 36 }).notNull().references(() => CardVersion.id),
   invoice: varchar('invoice', { length: 64 }).notNull().references(() => Invoice.paymentHash),
+}, (table) => {
+  return {
+    pk: primaryKey({ columns: [table.cardVersion, table.invoice] }),
+  }
 })
 
 export type CardVersionHasInvoice = typeof CardVersionHasInvoice.$inferSelect
