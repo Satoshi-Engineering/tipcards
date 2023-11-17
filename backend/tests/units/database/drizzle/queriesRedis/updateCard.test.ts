2f889255d0a9b16805f32209be2f5dcade7fa737
Thomas Schagerl <tom@satoshiengineering.com>
2023-11-17 10:28:10 +0100

    [FEATURE] add unit test for updating text+note via drizzle updateCard projects#751

---
+++ backend/tests/units/database/drizzle/queriesRedis/updateCard.test.ts
@@ -0,0 +1,52 @@
+import '../../../mocks/process.env'
+import {
+  addData,
+  insertCards, insertCardVersions,
+  insertInvoices, insertCardVersionInvoices,
+  insertLnurlPs, insertLnurlWs,
+  updateCardVesion,
+  insertOrUpdateInvoice,
+  insertOrUpdateCardVersionInvoice,
+} from '../mocks/queries'
+
+import { createCard, createCardVersion, createInvoice } from '../../../../drizzleData'
+import { createCard as createRedisCard, createInvoice as createRedisInvoice } from '../../../../redisData'
+
+import { updateCard } from '@backend/database/drizzle/queriesRedis'
+
+describe('updateCard', () => {
+  it('should update text and note', async () => {
+    const card = createCard()
+    const cardVersion = createCardVersion(card)
+    const { invoice, cardVersionsHaveInvoice } = createInvoice(100, cardVersion)
+    addData({
+      cards: [card],
+      cardVersions: [cardVersion],
+      invoices: [invoice],
+      cardVersionInvoices: [...cardVersionsHaveInvoice],
+    })
+
+    const cardRedis = createRedisCard()
+    cardRedis.cardHash = card.hash
+    cardRedis.text = 'some super text'
+    cardRedis.note = 'some crazy note'
+    const invoiceRedis = createRedisInvoice(100)
+    invoiceRedis.payment_hash = invoice.paymentHash
+    invoiceRedis.payment_request = invoice.paymentRequest
+    cardRedis.invoice = invoiceRedis
+
+    await updateCard(cardRedis)
+    expect(insertCards).not.toHaveBeenCalled()
+    expect(insertCardVersions).not.toHaveBeenCalled()
+    expect(insertInvoices).not.toHaveBeenCalled()
+    expect(insertCardVersionInvoices).not.toHaveBeenCalled()
+    expect(insertLnurlPs).not.toHaveBeenCalled()
+    expect(insertLnurlWs).not.toHaveBeenCalled()
+    expect(updateCardVesion).toHaveBeenCalledWith(expect.objectContaining({
+      textForWithdraw: 'some super text',
+      noteForStatusPage: 'some crazy note',
+    }))
+    expect(insertOrUpdateInvoice).toHaveBeenCalledTimes(1)
+    expect(insertOrUpdateCardVersionInvoice).toHaveBeenCalledTimes(1)
+  })
+})
