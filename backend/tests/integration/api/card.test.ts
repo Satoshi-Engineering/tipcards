a3c34dca506678e71e47f1c6e76083d6db9218dd
thespielplatz <informatics@gmx.net>
2023-12-22 13:05:37 +0100

    [TASK] cleaup projects#844

--- backend/tests/integration/api/card.test.ts
+++ backend/tests/integration/api/card.test.ts
@@ -14,37 +14,4 @@ describe('card', () => {
     expect(axios.isAxiosError(caughtError)).toBe(true)
     expect(caughtError?.response?.status).toBe(404)
   })
-
-  // todo : instead of creating the card via direct database connection create it using the api routes. test create + status step by step
-  /*
-  it('should return a card that exists in the database', async () => {
-    const card = createCard()
-    const cardVersion = createCardVersion(card)
-    await addData({
-      cards: [card],
-      cardVersions: [cardVersion],
-    })
-
-    let response: AxiosResponse
-    try {
-      response = await axios.get(`${process.env.TEST_API_ORIGIN}/api-drizzle/card/${card.hash}`)
-    } catch (error) {
-      console.error(error)
-      expect(false).toBe(true)
-      return
-    }
-    expect(response.data.data).toEqual(expect.objectContaining({
-      cardHash: card.hash,
-      text: cardVersion.textForWithdraw,
-      note: cardVersion.noteForStatusPage,
-      invoice: null,
-      lnurlp: null,
-      setFunding: null,
-      lnbitsWithdrawId: null,
-      landingPageViewed: null,
-      isLockedByBulkWithdraw: false,
-      used: null,
-    }))
-  })
-  */
 })
