07101c032aea254499f3863f551595380aac2222
Thomas Schagerl <tom@satoshiengineering.com>
2024-01-17 14:54:16 +0100

    [TASK] add exponential restart delay projects#873

--- backend/deploy/ecosystem.config.js
+++ backend/deploy/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     {
       name: 'lightning-tip-cards-backend',
       script: './backend/index.js',
+      exp_backoff_restart_delay: 1000,
       node_args: '-r tsconfig-paths/register',
       env: {
         NODE_ENV: 'production',
