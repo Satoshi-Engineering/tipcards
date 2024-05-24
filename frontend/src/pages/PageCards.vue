4d851859aae3dea4bf4d4a21116bb48e3404cbb3
dr-erych <dave@satoshiengineering.com>
2024-05-24 14:56:04 +0200

    [BUGFIX] hide amounts on printed cards projects#1042

--- frontend/src/pages/PageCards.vue
+++ frontend/src/pages/PageCards.vue
@@ -427,7 +427,7 @@
             </div>
             <div
               v-else-if="card.amount != null && card.status === 'used'"
-              class="absolute flex right-0.5 top-0.5 px-2 py-1 rounded-full bg-lightningpurple text-white text-xs break-anywhere"
+              class="absolute flex right-0.5 top-0.5 px-2 py-1 rounded-full bg-lightningpurple text-white text-xs break-anywhere print:hidden"
             >
               <span class="m-auto">{{ card.amount }} sats</span>
             </div>
