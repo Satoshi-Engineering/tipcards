56ee250d6e508b89e56add9ff61b4079e7e10c7c
Thomas Schagerl <tom@satoshiengineering.com>
2024-06-05 13:43:46 +0200

    [BUGFIX] prevent vite from adding process.env checks in externalCardStatus library projects#1039

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -51,6 +51,7 @@ export default defineConfig(({ mode }) => {
       __VUE_I18N_FULL_INSTALL__: true,
       __VUE_I18N_LEGACY_API__: false,
       __INTLIFY_PROD_DEVTOOLS__: false,
+      'process.env.NODE_ENV': JSON.stringify(mode),
     },
     build,
   }
