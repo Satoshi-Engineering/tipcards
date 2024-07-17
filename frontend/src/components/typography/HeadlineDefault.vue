7616a3bfbafe2d7cb286076eb538d09cc45ac022
dr-erych <dave@satoshiengineering.com>
2024-07-17 18:11:52 +0200

    [REFACTOR] move headline shared styles projects#1131

--- frontend/src/components/typography/HeadlineDefault.vue
+++ frontend/src/components/typography/HeadlineDefault.vue
@@ -44,11 +44,11 @@ const props = defineProps({
 })
 
 const classesByLevel: Record<string, string> = {
-  h1: 'text-3xl my-6 first:mt-0 last:mb-0 font-lato font-bold',
-  h2: 'text-2xl my-4 first:mt-0 last:mb-0 font-lato font-bold',
-  h3: 'text-xl my-3 first:mt-0 last:mb-0 font-lato font-bold',
-  h4: 'text-base my-3 first:mt-0 last:mb-0 font-lato font-bold',
+  h1: 'text-3xl my-6',
+  h2: 'text-2xl my-4',
+  h3: 'text-xl my-3',
+  h4: 'text-base my-3',
 }
 
-const classes = computed(() => `text-black ${classesByLevel[props.styling || props.level]}`)
+const classes = computed(() => `text-black first:mt-0 last:mb-0 font-lato font-bold ${classesByLevel[props.styling || props.level]}`)
 </script>
