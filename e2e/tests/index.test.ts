ec26b0482a63df0d7afd7492c7ef00c5becdef74
Thomas Schagerl <tom@satoshiengineering.com>
2024-07-18 13:15:20 +0200

    [TESTING] add e2e test for header home link projects#1143

--- e2e/tests/index.test.ts
+++ e2e/tests/index.test.ts
@@ -1,8 +1,31 @@
 // https://on.cypress.io/api
 
+const tipCards = new URL(Cypress.env('TIPCARDS_ORIGIN'))
+
+const urlWithOptionalTrailingSlash = (url: URL) => new RegExp(`${url}(\\/)?$`)
+
 describe('Web client', () => {
   it('visits the app root url and checks the headline', () => {
-    cy.visit(Cypress.env('TIPCARDS_ORIGIN'))
+    cy.visit(tipCards.href)
     cy.contains('h1', 'Lightning Tip Cards')
+  })
+
+  it('navigates to the style-guide page and back to home', () => {
+    cy.visit(new URL('/style-guide', tipCards).href)
+    cy.contains('h1', 'Lightning Tip Cards Style Guide')
+
+    // navigate home
+    cy.get('header > a:first-child').click()
+    cy.url().should('to.match', urlWithOptionalTrailingSlash(tipCards))
+
+    // navigate to english style guide and home from there
+    cy.visit(new URL('/en/style-guide', tipCards).href)
+    cy.get('header > a:first-child').click()
+    cy
+      .url()
+      .should(
+        'to.match',
+        urlWithOptionalTrailingSlash(new URL('/en', tipCards)),
+      )
   })
 })
