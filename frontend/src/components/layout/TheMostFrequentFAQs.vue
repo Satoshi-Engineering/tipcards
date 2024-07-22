ea2e209db78a1d1394498a4fe786cb17d912f634
Thomas Schagerl <tom@satoshiengineering.com>
2024-07-22 11:05:32 +0200

    [FEATURE] TheMostFrequentFAQs add question mark svg projects#1165

--- frontend/src/components/layout/TheMostFrequentFAQs.vue
+++ frontend/src/components/layout/TheMostFrequentFAQs.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="rounded-t-2xl bg-bluegrey">
-    <CenterContainer class="flex flex-col py-7 border-b border-white">
+    <CenterContainer class="relative flex flex-col py-7 border-b border-white">
+      <QuestionMark class="absolute -top-11 right-0 rtl:left-0 rtl:right-auto" />
       <HeadlineDefault level="h2" class="!text-white">
         {{ $t('footer.mostFrequentFAQs.title') }}
       </HeadlineDefault>
@@ -15,8 +16,9 @@
 </template>
 
 <script setup lang="ts">
-import CenterContainer from '@/components/layout/CenterContainer.vue'
 import ButtonDefault from '@/components/buttons/ButtonDefault.vue'
+import CenterContainer from '@/components/layout/CenterContainer.vue'
+import QuestionMark from '@/components/svgs/QuestionMark.vue'
 import HeadlineDefault from '@/components/typography/HeadlineDefault.vue'
 import ParagraphDefault from '@/components/typography/ParagraphDefault.vue'
 </script>
