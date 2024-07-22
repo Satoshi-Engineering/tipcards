8af01aaf1b8823356fb5ff8c3c79886467a96ec5
dr-erych <dave@satoshiengineering.com>
2024-07-22 12:24:44 +0200

    [BUGFIX] Make button icon rtl-ready projects#1167

--- frontend/src/components/buttons/ButtonDefaultIcon.vue
+++ frontend/src/components/buttons/ButtonDefaultIcon.vue
@@ -1,11 +1,11 @@
 <template>
   <span
     class="
-      absolute left-2 top-[50%] translate-y-[-50%]
-      w-[30px] h-[30px]
+      absolute start-2 top-1/2 -translate-y-1/2
+      w-9 h-9
       flex items-center justify-center
       pointer-events-none
-      transition-transform duration-300 group-hover:translate-x-1 group-active:translate-x-1
+      transition-transform duration-300 ltr:group-hover:translate-x-1 ltr:group-active:translate-x-1 rtl:group-hover:-translate-x-1 rtl:group-active:-translate-x-1
     "
   >
     <span
@@ -24,7 +24,7 @@
     />
     <svg
       v-else
-      class="w-4"
+      class="w-4 rtl:-scale-x-100"
       viewBox="0 0 14 12"
     >
       <path d="M.875 5.143h10.137L7.255 1.465a.846.846 0 0 1 0-1.214.889.889 0 0 1 1.239 0l5.249 5.142a.848.848 0 0 1 0 1.214l-5.25 5.142a.889.889 0 0 1-1.238 0 .846.846 0 0 1 0-1.214l3.757-3.678H.875A.866.866 0 0 1 0 6c0-.473.392-.857.875-.857z" fill="#FFF" />
