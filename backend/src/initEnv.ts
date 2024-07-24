eb925dcce1d5181ec973556cd74f8605d6ea395e
Thomas Schagerl <tom@satoshiengineering.com>
2024-07-24 09:54:33 +0200

    [REFACTOR] rename all env files (backend/frontend/e2e) to .env and .env.local for consistency. then use those files in pipeline integration/e2e tests to reduce configuration needed in gitlab projects#1173

--- backend/src/initEnv.ts
+++ backend/src/initEnv.ts
@@ -3,20 +3,24 @@ import path from 'path'
 
 // DO NOT IMPORT USING @backend AS THIS FILE IS ALSO USED BY DRIZZLE-KIT
 
+const loadEnvFile = (envFile: string) => {
+  config({
+    path: path.resolve(process.cwd(), envFile),
+    override: true, // we want to make sure that our configured values are always used
+  })
+  config({
+    path: path.resolve(process.cwd(), 'backend', envFile),
+    override: true, // we want to make sure that our configured values are always used
+  })
+}
+
+loadEnvFile('.env')
+loadEnvFile('.env.local')
+
 const argumentEnvIntegrationTest = process.argv.slice(2).find((value) => value.indexOf('--envIntegrationTest') === 0)
-let envFile = '.env'
 if (
   argumentEnvIntegrationTest
   || process.env.VITEST
 ) {
-  envFile = '.env.integrationTest'
+  loadEnvFile('.env.integrationTest')
 }
-
-config({
-  path: path.resolve(process.cwd(), envFile),
-  override: true, // we want to make sure that our configured values are always used
-})
-config({
-  path: path.resolve(process.cwd(), 'backend', envFile),
-  override: true, // we want to make sure that our configured values are always used
-})
