e2032837e84fb759b2a3f4effd85575182f6a421
dr-erych <dave@satoshiengineering.com>
2024-08-06 17:30:56 +0200

    [REFACTOR] rename placeholder projects#1189

--- frontend/src/components/FaqI18nT.vue
+++ frontend/src/components/FaqI18nT.vue
@@ -1,12 +1,12 @@
 <template>
   <I18nT :keypath="keypath">
-    <template #email>
+    <template #supportEmail>
       <LinkDefault :href="`mailto:${SUPPORT_EMAIL}?subject=Lightning%20Tip%20Cards%20Feedback`">{{ SUPPORT_EMAIL }}</LinkDefault>
     </template>
   </I18nT>
 </template>
 
-<script setup lang="tsx">
+<script setup lang="ts">
 import { I18nT } from 'vue-i18n'
 
 import LinkDefault from './typography/LinkDefault.vue'
