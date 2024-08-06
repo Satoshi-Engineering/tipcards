c9a16f019691af1d6e560473b76000daf5616f73
dr-erych <dave@satoshiengineering.com>
2024-08-06 11:00:34 +0200

    [TASK] Use HeroSection and buttons on about page projects#1189

--- frontend/src/pages/PageAbout.vue
+++ frontend/src/pages/PageAbout.vue
@@ -1,29 +1,46 @@
 <template>
   <TheLayout>
-    <CenterContainer>
-      <HeadlineDefault level="h1" styling="h2">
-        About Lightning TipCards
-      </HeadlineDefault>
-      <ParagraphDefault>
-        Lightning TipCards are a tip (or gift) card system, where you can fund tip cards via the lightning network and the gifted person can redeem the funded cards. The gifted person gets a short introduction to bitcoin and a lightning app recommendation to start their journey into the bitcoin rabbit hole.
-      </ParagraphDefault>
+    <HeroSection>
+      <template #default>
+        <HeadlineDefault level="h1" styling="h2">
+          About Lightning TipCards
+        </HeadlineDefault>
+        <ParagraphDefault>
+          Lightning TipCards are a tip (or gift) card system, where you can fund tip cards via the lightning network and the gifted person can redeem the funded cards. The gifted person gets a short introduction to bitcoin and a lightning app recommendation to start their journey into the bitcoin rabbit hole.
+        </ParagraphDefault>
+      </template>
+      <template #buttonContainer>
+        <ButtonDefault
+          :to="{ name: 'cards'}"
+        >
+          Get started
+        </ButtonDefault>
+      </template>
+    </HeroSection>
+    <CenterContainer class="my-10">
       <HeadlineDefault level="h2">
         Open Source
       </HeadlineDefault>
       <ParagraphDefault>
         You are free to setup your own version of Lightning TipCards or to support us by creating issues, contributing translations, bug fixes or new features via Pull Requests on Github.
       </ParagraphDefault>
-      <ParagraphDefault class="text-center my-12">
+      <ParagraphDefault class="text-center !my-8">
         <LinkDefault
           href="https://github.com/Satoshi-Engineering/tip-cards"
           title="Lightning TipCards on Github"
+          class="group"
         >
           <!-- eslint-disable-next-line vue/multiline-html-element-content-newline, vue/max-attributes-per-line, vue/html-self-closing -->
           <svg class="w-20 inline-block" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
-          <br>
-          TipCards on Github
         </LinkDefault>
       </ParagraphDefault>
+      <ButtonContainer>
+        <ButtonDefault href="https://github.com/Satoshi-Engineering/tip-cards">
+          TipCards on Github
+        </ButtonDefault>
+      </ButtonContainer>
+    </CenterContainer>
+    <CenterContainer class="my-10">
       <HeadlineDefault level="h2">
         Terms of service
       </HeadlineDefault>
@@ -33,9 +50,13 @@
       <ParagraphDefault>
         This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
       </ParagraphDefault>
-      <ParagraphDefault>
-        <LinkDefault href="https://github.com/Satoshi-Engineering/tip-cards/blob/main/LICENSE">See the full license terms for more details.</LinkDefault>
-      </ParagraphDefault>
+      <ButtonContainer class="my-8">
+        <ButtonDefault href="https://github.com/Satoshi-Engineering/tip-cards/blob/main/LICENSE">
+          Full license terms
+        </ButtonDefault>
+      </ButtonContainer>
+    </CenterContainer>
+    <CenterContainer class="my-10">
       <HeadlineDefault level="h2" class="mt-8">
         Custodial service
       </HeadlineDefault>
@@ -59,4 +80,7 @@ import CenterContainer from '@/components/layout/CenterContainer.vue'
 import HeadlineDefault from '@/components/typography/HeadlineDefault.vue'
 import ParagraphDefault from '@/components/typography/ParagraphDefault.vue'
 import LinkDefault from '@/components/typography/LinkDefault.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import ButtonDefault from '@/components/buttons/ButtonDefault.vue'
+import ButtonContainer from '@/components/buttons/ButtonContainer.vue'
 </script>
