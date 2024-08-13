b2b8009699c4fd805ef5ca9730e07cd3266706a1
Thomas Schagerl <tom@satoshiengineering.com>
2024-08-13 10:28:34 +0200

    [BUGFIX] e2e/lib/urlHelpers remove trailing slash before optionally adding it

--- e2e/lib/urlHelpers.ts
+++ e2e/lib/urlHelpers.ts
@@ -1,1 +1,7 @@
-export const urlWithOptionalTrailingSlash = (url: URL) => new RegExp(`${url}(\\/)?$`)
+export const urlWithOptionalTrailingSlash = (url: URL) => {
+  let urlWithoutTrailingSlash = `${url}`
+  while (urlWithoutTrailingSlash.slice(-1) === '/') {
+    urlWithoutTrailingSlash = urlWithoutTrailingSlash.slice(0, -1)
+  }
+  return new RegExp(`${urlWithoutTrailingSlash}(\\/)?$`)
+}
