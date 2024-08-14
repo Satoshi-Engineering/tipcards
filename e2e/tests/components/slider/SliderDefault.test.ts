dc0e51007b3b29f28be96f2ba9560ce1c0d465f2
Thomas Schagerl <tom@satoshiengineering.com>
2024-08-14 14:46:57 +0200

    [BUGFIX] SliderDefault swipe more than 50% width of the slider to make sure it always slides to the next slide projects#1233

--- e2e/tests/components/slider/SliderDefault.test.ts
+++ e2e/tests/components/slider/SliderDefault.test.ts
@@ -1,57 +1,64 @@
 import { TIPCARDS_ORIGIN } from '@e2e/lib/constants'
 
+const x = 1000
+
+const pointerDownEvent = {
+  button: 0,
+  buttons: 1,
+  pointerId: 1,
+  clientX: x,
+  clientY: 1000,
+  screenX: x,
+  screenY: 1000,
+  pageX: x,
+  pageY: 1000,
+}
+
+const pointerMoveLeft = (deltaX: number) => ({
+  pointerId: 1,
+  clientX: x - deltaX,
+  clientY: 1000,
+  screenX: x - deltaX,
+  screenY: 1000,
+  pageX: x - deltaX,
+  pageY: 1000,
+})
+
 describe('SliderDefault', () => {
   it('renders the slider and swipes to the second slide', () => {
     cy.visit(new URL('/style-guide/components', TIPCARDS_ORIGIN).href)
+    cy.get('[data-test="slider-default"]').first().as('slider')
+    cy.get('@slider').find('[data-test="slide-default"]').eq(0).as('slide1')
+    cy.get('@slider').find('[data-test="slide-default"]').eq(1).as('slide2')
+    cy.get('@slider').find('[data-test="slide-default"]').eq(2).as('slide3')
 
     // renders the slider
-    cy.get('[data-test="slide-default"]').first().should('be.visible')
-    cy.get('[data-test="slide-default"]').first().should('contain', 'Slide 1')
-    cy.get('[data-test="slide-default"]').eq(1).should('not.be.visible')
-    cy.get('[data-test="slide-default"]').eq(2).should('not.be.visible')
+    cy.get('@slide1')
+      .should('be.visible')
+      .should('contain', 'Slide 1')
+    cy.get('@slide2').should('not.be.visible')
+    cy.get('@slide3').should('not.be.visible')
 
     // swipes to the second slide
-    cy.get('[data-test="slider-default"]').first().find('ul')
-      .trigger('pointerdown', {
-        button: 0,
-        buttons: 1,
-        pointerId: 1,
-        clientX: 1000,
-        clientY: 1000,
-        screenX: 1000,
-        screenY: 1000,
-        pageX: 1000,
-        pageY: 1000,
-      })
-      .trigger('pointermove', {
-        pointerId: 1,
-        clientX: 900,
-        clientY: 1000,
-        screenX: 900,
-        screenY: 1000,
-        pageX: 900,
-        pageY: 1000,
-      })
-      .trigger('pointermove', {
-        pointerId: 1,
-        clientX: 800,
-        clientY: 1000,
-        screenX: 800,
-        screenY: 1000,
-        pageX: 800,
-        pageY: 1000,
-      })
-      .trigger('pointerup', { force: true, pointerId: 1 })
+    cy.get('@slider').then(($slider) => {
+      const width = $slider.width()
+      cy.log(`Width: ${width}`)
+      cy.get('@slider').find('ul')
+        .trigger('pointerdown', pointerDownEvent)
+        .trigger('pointermove', pointerMoveLeft(Math.round(width * 0.3)))
+        .trigger('pointermove', pointerMoveLeft(Math.round(width * 0.6)))
+        .trigger('pointerup', { force: true, pointerId: 1 })
+    })
     cy.wait(500)
-    cy.get('[data-test="slide-default"]').first().should('not.be.visible')
-    cy.get('[data-test="slide-default"]').eq(1).should('be.visible')
-    cy.get('[data-test="slide-default"]').eq(2).should('not.be.visible')
+    cy.get('@slide1').should('not.be.visible')
+    cy.get('@slide2').should('be.visible')
+    cy.get('@slide3').should('not.be.visible')
 
     // navigates to third slide using pagination
     cy.get('[data-test="slider-default-pagination"] button').eq(2).click()
     cy.wait(500)
-    cy.get('[data-test="slide-default"]').first().should('not.be.visible')
-    cy.get('[data-test="slide-default"]').eq(1).should('not.be.visible')
-    cy.get('[data-test="slide-default"]').eq(2).should('be.visible')
+    cy.get('@slide1').should('not.be.visible')
+    cy.get('@slide2').should('not.be.visible')
+    cy.get('@slide3').should('be.visible')
   })
 })
