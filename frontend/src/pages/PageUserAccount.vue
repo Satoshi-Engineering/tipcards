241c30d60ea15b32419240d88c2c82c2448816f6
Thomas Schagerl <tom@satoshiengineering.com>
2024-08-16 13:32:36 +0200

    [REFACTOR] PageUserAccount use new store i/o making request to auth api projects#1235

--- frontend/src/pages/PageUserAccount.vue
+++ frontend/src/pages/PageUserAccount.vue
@@ -5,7 +5,7 @@
         <HeadlineDefault level="h1" class="text-center">
           {{ $t('userAccount.title') }}
         </HeadlineDefault>
-        <form class="block my-10" @submit.prevent="save">
+        <form class="block my-10" @submit.prevent="update(profileInternal)">
           <label class="block mb-2">
             <span class="block">
               {{ $t('userAccount.form.accountName') }}:
@@ -14,7 +14,7 @@
               v-model.lazy.trim="profileInternal.accountName"
               type="text"
               class="w-full border my-1 px-3 py-2 focus:outline-none"
-              :disabled="fetching || saving"
+              :disabled="fetching"
             >
             <small class="block">({{ $t('userAccount.form.accountNameHint') }})</small>
           </label>
@@ -26,7 +26,7 @@
               v-model.lazy.trim="profileInternal.displayName"
               type="text"
               class="w-full border my-1 px-3 py-2 focus:outline-none"
-              :disabled="fetching || saving"
+              :disabled="fetching"
             >
             <small class="block">({{ $t('userAccount.form.displayNameHint') }})</small>
           </label>
@@ -38,24 +38,23 @@
               v-model.lazy.trim="profileInternal.email"
               type="email"
               class="w-full border my-1 px-3 py-2 focus:outline-none"
-              :disabled="fetching || saving"
+              :disabled="fetching"
             >
             <small class="block">({{ $t('userAccount.form.emailAddressHint') }})</small>
           </label>
           <ButtonContainer>
             <ButtonDefault
               type="submit"
-              :disabled="fetching || saving"
-              :loading="saving"
+              :loading="fetching"
             >
               <span>
                 {{ $t('userAccount.form.save') }}
               </span>
-              <IconCheckSquareFill v-if="isSaved && !saving" class="inline-block ms-2 h-[1em] w-[1em]" />
-              <IconExclamationSquare v-else-if="!saving" class="inline-block ms-2 h-[1em] w-[1em]" />
-              <span v-else-if="saving" class="inline-block ms-2 h-[1em] w-[1em]" />
+              <IconCheckSquareFill v-if="isSaved && !fetching" class="inline-block ms-2 h-[1em] w-[1em]" />
+              <IconExclamationSquare v-else-if="!fetching" class="inline-block ms-2 h-[1em] w-[1em]" />
+              <span v-else-if="fetching" class="inline-block ms-2 h-[1em] w-[1em]" />
             </ButtonDefault>
-            <UserErrorMessages :user-error-messages="profileUserErrorMessages" />
+            <UserErrorMessages :user-error-messages="fetchingUserErrorMessages" />
           </ButtonContainer>
         </form>
       </CenterContainer>
@@ -88,14 +87,17 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, computed, watchEffect, onBeforeMount } from 'vue'
+import { ref, reactive, computed, watchEffect, onBeforeMount, onBeforeUnmount } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import isEqual from 'lodash.isequal'
 import { useI18n } from 'vue-i18n'
 import { storeToRefs } from 'pinia'
 
-import { Profile } from '@shared/data/auth/User'
+import { Profile } from '@shared/data/trpc/Profile'
 
+import IconCheckSquareFill from '@/components/icons/IconCheckSquareFill.vue'
+import IconExclamationSquare from '@/components/icons/IconExclamationSquare.vue'
+import ButtonContainer from '@/components/buttons/ButtonContainer.vue'
 import TheLayout from '@/components/layout/TheLayout.vue'
 import CenterContainer from '@/components/layout/CenterContainer.vue'
 import ButtonDefault from '@/components/buttons/ButtonDefault.vue'
@@ -103,9 +105,7 @@ import HeadlineDefault from '@/components/typography/HeadlineDefault.vue'
 import UserErrorMessages from '@/components/UserErrorMessages.vue'
 import useAuthService from '@/modules/useAuthService'
 import { useAuthStore } from '@/stores/auth'
-import IconCheckSquareFill from '@/components/icons/IconCheckSquareFill.vue'
-import IconExclamationSquare from '@/components/icons/IconExclamationSquare.vue'
-import ButtonContainer from '@/components/buttons/ButtonContainer.vue'
+import useProfile from '@/stores/useProfile'
 
 const router = useRouter()
 const route = useRoute()
@@ -115,38 +115,35 @@ const { isLoggedIn } = storeToRefs(authStore)
 const { t } = useI18n()
 const authService = useAuthService()
 
-const profile = ref(Profile.parse({}))
-const profileInternal = reactive(Profile.parse({}))
-const profileUserErrorMessages = ref<string[]>([])
+const {
+  userAccountName,
+  userDisplayName,
+  userEmail,
+  fetching,
+  fetchingUserErrorMessages,
+  subscribe,
+  unsubscribe,
+  update,
+} = useProfile()
 
-const fetching = ref(false)
+const profileInternal = reactive(Profile.partial().parse({}))
+
 onBeforeMount(async () => {
-  fetching.value = true
   try {
-    const { data } = await authService.get('profile')
-    profile.value = Profile.parse(data.data)
-    Object.assign(profileInternal, profile.value)
+    await subscribe()
+    profileInternal.accountName = userAccountName.value
+    profileInternal.displayName = userDisplayName.value
+    profileInternal.email = userEmail.value
   } catch (error) {
-    console.error(error)
-    profileUserErrorMessages.value.push(t('userAccount.errors.unableToFetchProfile'))
+    // do nothing
   }
-  fetching.value = false
 })
-const isSaved = computed(() => isEqual(profile.value, profileInternal))
-
-const saving = ref(false)
-const save = async () => {
-  profileUserErrorMessages.value = []
-  saving.value = true
-  try {
-    const { data } = await authService.post('profile', profileInternal)
-    profile.value = Profile.parse(data.data)
-  } catch (error) {
-    console.error(error)
-    profileUserErrorMessages.value.push(t('userAccount.errors.unableToSaveProfile'))
-  }
-  saving.value = false
-}
+onBeforeUnmount(unsubscribe)
+const isSaved = computed(() => isEqual({
+  accountName: userAccountName.value,
+  displayName: userDisplayName.value,
+  email: userEmail.value,
+}, profileInternal))
 
 const logoutUserErrorMessages = ref<string[]>([])
 
@@ -174,6 +171,21 @@ const logoutAllOtherDevices = async () => {
 watchEffect(() => {
   if (isLoggedIn.value === false) {
     router.push({ name: 'home', params: { lang: route.params.lang } })
+  }
+})
+watchEffect(() => {
+  if (userAccountName.value != null && profileInternal.accountName == null) {
+    profileInternal.accountName = userAccountName.value
+  }
+})
+watchEffect(() => {
+  if (userDisplayName.value != null && profileInternal.displayName == null) {
+    profileInternal.displayName = userDisplayName.value
+  }
+})
+watchEffect(() => {
+  if (userEmail.value != null && profileInternal.email == null) {
+    profileInternal.email = userEmail.value
   }
 })
 </script>
