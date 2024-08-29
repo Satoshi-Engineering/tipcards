ecc6c0ba8f97b18d05d10b7463b1b3929decc0a8
Thomas Schagerl <tom@satoshiengineering.com>
2024-08-29 09:35:05 +0200

    [TASK] auth.logoutAllOtherDevices.test.ts increase delay between refreshes of multiple frontends projects#1235

--- backend/tests/integration/api/auth.logoutAllOtherDevices.test.ts
+++ backend/tests/integration/api/auth.logoutAllOtherDevices.test.ts
@@ -15,24 +15,35 @@ const failEarly = new FailEarly(it)
 
 const NUM_OF_FRONTENDS = 4
 const randomMnemonic = HDWallet.generateRandomMnemonic()
-const multipleFrondendSimulatorsWithSameSigningDevice: FrontendSimulator[] = new Array(NUM_OF_FRONTENDS).fill(0).map(() => new FrontendSimulator(randomMnemonic))
+const multipleFrondendSimulatorsWithSameSigningDevice: FrontendSimulator[] = new Array(NUM_OF_FRONTENDS)
+  .fill(0)
+  .map(() => new FrontendSimulator(randomMnemonic))
 
 describe('logout all other devices', () => {
   failEarly.it('should login all frontends', async () => {
     for (const frontend of multipleFrondendSimulatorsWithSameSigningDevice) {
       await frontend.login()
-      // NOTE: Redis specifiy problem: Add delay to prevent backend to write the same user object at the same time. Currently redis has no data lock on user object.
-      await delay(250)
+      // NOTE
+      // In redis the refresh tokens were stored in the user object. Even after the switch to drizzle we still
+      // build a temporary user object (in the application) that holds all refresh tokens.
+      // This is why we need to add a delay to prevent backend to write the same user object at the same time.
+      // Also the backend has no data lock on user object.
+      await delay(500)
     }
   })
 
-  failEarly.it('shoud check if all frontends have different refresh & access Tokens', async () => {
-    const refreshTokens = multipleFrondendSimulatorsWithSameSigningDevice.map(f => f.refreshToken)
+  failEarly.it('should check if all frontends have the same userId but different refresh & access tokens', async () => {
+    const userIds = multipleFrondendSimulatorsWithSameSigningDevice.map((frontend) => frontend.userId)
+    const userIdsAsSet = new Set(userIds)
+
+    expect(userIdsAsSet.size).toBe(1)
+
+    const refreshTokens = multipleFrondendSimulatorsWithSameSigningDevice.map((frontend) => frontend.refreshToken)
     const refreshTokensAsSet = new Set(refreshTokens)
 
     expect(refreshTokensAsSet.size).toBe(refreshTokens.length)
 
-    const accessTokens = multipleFrondendSimulatorsWithSameSigningDevice.map(f => f.accessToken)
+    const accessTokens = multipleFrondendSimulatorsWithSameSigningDevice.map((frontend) => frontend.accessToken)
     const accessTokensAsSet = new Set(refreshTokens)
 
     expect(accessTokensAsSet.size).toBe(accessTokens.length)
@@ -41,12 +52,16 @@ describe('logout all other devices', () => {
   failEarly.it('should refresh all frontends', async () => {
     for (const frontend of multipleFrondendSimulatorsWithSameSigningDevice) {
       await frontend.authRefresh()
-      // NOTE: Redis specifiy problem: Add delay to prevent backend to write the same user object at the same time. Currently redis has no data lock on user object.
-      await delay(250)
+      // NOTE
+      // In redis the refresh tokens were stored in the user object. Even after the switch to drizzle we still
+      // build a temporary user object (in the application) that holds all refresh tokens.
+      // This is why we need to add a delay to prevent backend to write the same user object at the same time.
+      // Also the backend has no data lock on user object.
+      await delay(500)
     }
   })
 
-  failEarly.it('shoud check if all frontends still have different refresh & access Tokens', async () => {
+  failEarly.it('should check if all frontends still have different refresh & access tokens', async () => {
     const refreshTokens = multipleFrondendSimulatorsWithSameSigningDevice.map(f => f.refreshToken)
     const refreshTokensAsSet = new Set(refreshTokens)
 
@@ -60,16 +75,17 @@ describe('logout all other devices', () => {
 
   failEarly.it('should logout all frontends, except the first one', async () => {
     const frontend = multipleFrondendSimulatorsWithSameSigningDevice[0]
-
     // make sure two auth tokens exist
     await frontend.authRefresh()
+
     const response = await frontend.logoutAllOtherDevices()
+
     expect(response.data).toEqual(expect.objectContaining({
       status: 'success',
     }))
   })
 
-  failEarly.it('should fail the refresh all frontends, except the first one', async () => {
+  failEarly.it('should fail to refresh all frontends, except for the first one', async () => {
     const loggedInFrontend = multipleFrondendSimulatorsWithSameSigningDevice[0]
     const loggedOutFrontends = multipleFrondendSimulatorsWithSameSigningDevice.slice(1)
 
