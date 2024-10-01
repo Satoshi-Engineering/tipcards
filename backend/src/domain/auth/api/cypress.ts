ed709c70fae7e8c6e0431dc6ce3ac19583848f6b
thespielplatz <informatics@gmx.net>
2024-10-01 11:23:35 +0200

    [TASK] refactor cypress api to use trpc and depricate backend/auth/api/cypress apis projects#1229

--- backend/src/domain/auth/api/cypress.ts
+++ backend/src/domain/auth/api/cypress.ts
@@ -1,86 +1,10 @@
-// Since Cypress isn't fully compatible with tRPC and implementing automated login would require too much effort, we'll handle some backend calls directly here.
-
 import { Router } from 'express'
-import cookieParser from 'cookie-parser'
 
 import HDWallet from '@shared/modules/HDWallet/HDWallet.js'
-import { ErrorCode } from '@shared/data/Errors.js'
-
-import Auth from '@backend/domain/auth/Auth.js'
-import { getUserById, updateUser } from '@backend/database/deprecated/queries.js'
 
 const router = Router()
 
-/////
-// LnurlAuth
-//
-
-router.get('/lnurlAuth/create', async (_, res) => {
-  const lnurlAuthLogin = Auth.getAuth().getLnurlAuthLogin()
-  if (lnurlAuthLogin == null) {
-    res.json({
-      status: 'error',
-      message: 'LnurlAuthLogin not initialized.',
-    }).status(500)
-    return
-  }
-
-  const result = await lnurlAuthLogin.create()
-
-  res.json({
-    status: 'success',
-    data: {
-      encoded: result.lnurlAuth,
-      hash: result.hash,
-    },
-  })
-})
-
-router.get('/loginWithLnurlAuthHash/:hash', async (req, res) => {
-  const hash = req.params.hash
-  const result = await Auth.getAuth().loginWithLnurlAuthHash(hash)
-  res
-    .cookie('refresh_token', result.refreshToken, {
-      expires: new Date(+ new Date() + 1000 * 60 * 60 * 24 * 365),
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-    })
-    .json({
-      status: 'success',
-      data: { accessToken: result.accessToken },
-    })
-})
-
-
-router.post('/logout', cookieParser(), async (req, res) => {
-  const oldRefreshToken = req.cookies?.refresh_token
-  res.clearCookie('refresh_token', {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-  })
-  if (oldRefreshToken != null) {
-    try {
-      const { id } = JSON.parse(atob(oldRefreshToken.split('.')[1]))
-      const user = await getUserById(id)
-      if (user?.allowedRefreshTokens != null) {
-        user.allowedRefreshTokens = user.allowedRefreshTokens
-          .filter((currentRefreshTokens) => !currentRefreshTokens.includes(oldRefreshToken))
-        await updateUser(user)
-      }
-    } catch (error) {
-      console.error(ErrorCode.UnknownDatabaseError, error)
-      res.status(403).json({
-        status: 'error',
-        data: 'unknown database error',
-      })
-      return
-    }
-  }
-  res.json({ status: 'success' })
-})
-
+// Due to the numerous errors caused by importing the crypto libraries in the Cypress controlled browser, the private/public key generation was shifted to the backend.
 router.get('/createRandomPublicPrivateKeyPairAsHex', async (_, res) => {
   const randomMnemonic = HDWallet.generateRandomMnemonic()
   const hdWallet = new HDWallet(randomMnemonic)
--- e2e/lib/api/auth.ts
+++ e2e/lib/api/auth.ts
@@ -5,8 +5,8 @@ import { TIPCARDS_AUTH_ORIGIN } from '@e2e/lib/constants'
 import LNURLAuth from '@shared/modules/LNURL/LNURLAuth'
 
 const API_CYPRESS_CREATE_AUTH_KEYS = new URL('/auth/api/cypress/createRandomPublicPrivateKeyPairAsHex', TIPCARDS_AUTH_ORIGIN)
-const API_AUTH_CREATE = new URL('/auth/api/cypress/lnurlAuth/create', TIPCARDS_AUTH_ORIGIN)
-const API_AUTH_LOGIN = new URL('/auth/api/cypress/loginWithLnurlAuthHash', TIPCARDS_AUTH_ORIGIN)
+const API_AUTH_CREATE = new URL('/auth/trpc/lnurlAuth.create', TIPCARDS_AUTH_ORIGIN)
+const API_AUTH_LOGIN = new URL('/auth/trpc/auth.loginWithLnurlAuthHash', TIPCARDS_AUTH_ORIGIN)
 const API_AUTH_REFRESH = new URL('/api/auth/refresh', TIPCARDS_AUTH_ORIGIN)
 
 export const login = () => {
@@ -15,9 +15,10 @@ export const login = () => {
   cy.request({
     url: API_AUTH_CREATE.href,
   }).then((response) => {
-    expect(response.body).to.have.nested.property('data.hash')
-    const authServiceLoginHash = response.body.data.hash
-    const lnurl = response.body.data.encoded
+    expect(response.body).to.have.nested.property('result.data.json.hash')
+    expect(response.body).to.have.nested.property('result.data.json.lnurlAuth')
+    const authServiceLoginHash = response.body.result.data.json.hash
+    const lnurl = response.body.result.data.json.lnurlAuth
 
     cy.get('@lnurlAuth').then(function () {
       const LNURLAuthCallbackUrl = this.lnurlAuth.getLNURLAuthCallbackUrl(lnurl)
@@ -26,15 +27,29 @@ export const login = () => {
       }).its('status').should('eq', 200)
     })
 
-    cy.request({
-      url: `${API_AUTH_LOGIN.href}/${authServiceLoginHash}`,
-    }).then((response) => {
-      expect(response.body).to.have.nested.property('data.accessToken')
-      cy.wrap(response.body.data.accessToken).as('accessToken')
+    cy.then(() => {
+      const input = JSON.stringify({
+        0: {
+          json: {
+            hash: authServiceLoginHash,
+          },
+        },
+      })
+
+      cy.request({
+        url: API_AUTH_LOGIN.href,
+        qs: {
+          batch: 1,
+          input,
+        },
+      }).then((response) => {
+        expect(response.body).to.have.nested.property('[0].result.data.json.accessToken')
+        cy.wrap(response.body[0].result.data.json.accessToken).as('accessToken')
+      })
+      cy.getCookie('refresh_token', {
+        domain: TIPCARDS_AUTH_ORIGIN.hostname,
+      }).should('exist')
     })
-    cy.getCookie('refresh_token', {
-      domain: TIPCARDS_AUTH_ORIGIN.hostname,
-    }).should('exist')
   })
 }
 
@@ -71,7 +86,7 @@ export const clearAuth = () => {
 }
 
 export const createAndWrapLNURLAuth = () => {
-  // Due to the numerous errors caused by importing the crypto libraries in the Cypress browser, the private/public key generation was shifted to the backend.
+  // Due to the numerous errors caused by importing the crypto libraries in the Cypress controlled browser, the private/public key generation was shifted to the backend.
   cy.request({
     url: API_CYPRESS_CREATE_AUTH_KEYS.href,
   }).then((response) => {
