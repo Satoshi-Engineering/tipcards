a62996184760f48584f82a6f5152ab5f4c8e5076
thespielplatz <informatics@gmx.net>
2024-10-18 15:47:53 +0200

    [TASK] add unit test for shared/src/modules/Jwt/JwtIssuer.ts projects#1301

--- shared/tests/units/mocks/jose.ts
+++ shared/tests/units/mocks/jose.ts
@@ -2,6 +2,20 @@ import { vi } from 'vitest'
 
 const jose = await vi.importActual('jose')
 
+let signedReturnValue = ''
+export const setSignedValue = (value: string) => {
+  signedReturnValue = value
+}
+
+export const SignJWT = vi.fn(() => ({
+  setProtectedHeader: vi.fn().mockReturnThis(),
+  setIssuedAt: vi.fn().mockReturnThis(),
+  setIssuer: vi.fn().mockReturnThis(),
+  setAudience: vi.fn().mockReturnThis(),
+  setExpirationTime: vi.fn().mockReturnThis(),
+  sign: vi.fn().mockResolvedValue(signedReturnValue),
+}))
+
 vi.mock('jose', () => ({
   importSPKI: vi.fn(),
   importPKCS8: vi.fn(),
@@ -9,5 +23,6 @@ vi.mock('jose', () => ({
   exportPKCS8: vi.fn(),
   generateKeyPair: vi.fn(),
   jwtVerify: vi.fn(),
+  SignJWT,
   errors: jose.errors,
 }))
---
+++ shared/tests/units/modules/Jwt/JwtIssuer.test.ts
@@ -0,0 +1,43 @@
+import { describe, it, expect, vi } from 'vitest'
+
+import { setSignedValue } from '../../mocks/jose.js'
+import * as jose from 'jose'
+
+import JwtIssuer from '@shared/modules/Jwt/JwtIssuer.js'
+import { Algorithms } from '@shared/modules/Jwt/types/Algorithms.js'
+import { KeyPair } from '@shared/modules/Jwt/types/KeyPair.js'
+
+describe('JwtIssuer', () => {
+  const mockPublicKey = 'mockPublicKey'
+  const mockPrivateKey = 'mockPrivateKey'
+  const mockSpkiPublicKey = 'mockSpkiPublicKey'
+  const mockIssuer = 'mockIssuer'
+  const mockSignedJwt = 'mockSignedJwt'
+  const mockAlgorithm = Algorithms.RS256
+  const mockKeyPair: KeyPair = {
+    publicKey: mockPublicKey as unknown as jose.KeyLike,
+    privateKey: mockPrivateKey as unknown as jose.KeyLike,
+  }
+
+  const jwtIssuer = new JwtIssuer(mockKeyPair, mockIssuer, mockAlgorithm)
+
+  it('should create a JWT with the correct parameters', async () => {
+    const mockAudience = 'mockAudience'
+    const mockExpirationTime = '1h'
+    const mockPayload = { sub: '1234567890', name: 'John Doe', admin: true }
+    setSignedValue(mockSignedJwt)
+
+    const result = await jwtIssuer.createJwt(mockAudience, mockExpirationTime, mockPayload)
+
+    expect(result).toBe(mockSignedJwt)
+  })
+
+  it('should return the public key in SPKI format', async () => {
+    vi.spyOn(jose, 'exportSPKI').mockResolvedValueOnce(mockSpkiPublicKey)
+
+    const result = await jwtIssuer.getPublicKeyAsSPKI()
+
+    expect(jose.exportSPKI).toHaveBeenCalledWith(mockPublicKey)
+    expect(result).toBe('mockSpkiPublicKey')
+  })
+})
