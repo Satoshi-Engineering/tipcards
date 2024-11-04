e30e307ab3429665ecab89b74b4c67e9df1ebe9b
dr-erych <dave@satoshiengineering.com>
2024-11-04 15:32:47 +0100

    [BUGFIX] switch to EN for webClientRoot test

--- e2e/live-checks/webClientRoot.test.ts
+++ e2e/live-checks/webClientRoot.test.ts
@@ -1,8 +1,11 @@
 import { TIPCARDS_ORIGIN } from '@e2e/lib/constants'
+import { switchBrowserLanguageToEnglish } from '@e2e/lib/pages/utils'
 
 describe('Web client', () => {
   it('visits the app root url and checks the headline', () => {
-    cy.visit(TIPCARDS_ORIGIN.href)
+    cy.visit(TIPCARDS_ORIGIN.href, {
+      onBeforeLoad: switchBrowserLanguageToEnglish,
+    })
     cy.contains('h1', 'The easiest way to tip with Bitcoin')
   })
 })
