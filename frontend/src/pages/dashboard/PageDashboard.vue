9dca4662ab547fb2756be8289242c11165422ed6
dr-erych <dave@satoshiengineering.com>
2024-11-13 13:38:03 +0100

    [TASK] handle logged out on dashboard greeting projects#1336

--- frontend/src/pages/dashboard/PageDashboard.vue
+++ frontend/src/pages/dashboard/PageDashboard.vue
@@ -13,11 +13,20 @@
               {{ $t('dashboard.welcome') }}
             </HeadlineDefault>
             <LinkDefault
+              v-if="isLoggedIn"
               :to="{ name: 'user-account' }"
               no-bold
               class="text-lg"
             >
               {{ userDisplayName || $t('userAccount.completeProfile') }}
+            </LinkDefault>
+            <LinkDefault
+              v-else
+              no-bold
+              class="text-lg"
+              @click="showModalLogin = true"
+            >
+              {{ $t('general.login') }}
             </LinkDefault>
           </div>
         </div>
@@ -27,15 +36,24 @@
   </TheLayout>
 </template>
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+
 import CardsSummary from '@/components/CardsSummary.vue'
 import IconPersonCircleFilled from '@/components/icons/IconPersonCircleFilled.vue'
 import CenterContainer from '@/components/layout/CenterContainer.vue'
 import TheLayout from '@/components/layout/TheLayout.vue'
 import HeadlineDefault from '@/components/typography/HeadlineDefault.vue'
 import LinkDefault from '@/components/typography/LinkDefault.vue'
+import { useAuthStore } from '@/stores/auth'
 import { useProfileStore } from '@/stores/profile'
-import { storeToRefs } from 'pinia'
+import { useModalLoginStore } from '@/stores/modalLogin'
 
 const profileStore = useProfileStore()
 const { userDisplayName } = storeToRefs(profileStore)
+
+const authStore = useAuthStore()
+const { isLoggedIn } = storeToRefs(authStore)
+
+const modalLoginStore = useModalLoginStore()
+const { showModalLogin } = storeToRefs(modalLoginStore)
 </script>
