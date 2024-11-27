420b9fcb78911e750575a09491da2efb7d2ed86b
thespielplatz <informatics@gmx.net>
2024-11-27 15:30:06 +0100

    refactor(backend): Remove unused code
    - Remove AccessTokenFromResponse
    - Remove shared/src/data/Response.ts
    
    Refs: projects#1349

--- shared/src/data/auth/index.ts
+++ shared/src/data/auth/index.ts
@@ -1,7 +1,6 @@
 import z from 'zod'
 
 import { PermissionsEnum } from './User.js'
-import { AccessTokenResponse } from '../Response.js'
 
 export const AccessTokenPayload = z.object({
   userId: z.string(),
@@ -14,7 +13,3 @@ export type AccessTokenPayload = z.infer<typeof AccessTokenPayload>
 export const AccessToken = z.string()
 
 export type AccessToken = z.infer<typeof AccessToken>
-
-export const AccessTokenFromResponse = AccessTokenResponse.transform((response) => AccessToken.parse(response.data.accessToken))
-
-export type AccessTokenFromResponse = z.infer<typeof AccessTokenFromResponse>
