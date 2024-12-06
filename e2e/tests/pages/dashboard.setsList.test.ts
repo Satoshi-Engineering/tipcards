99b12428f70eb9a6c27d4999752ddcd6849c76aa
Thomas Schagerl <tom@satoshiengineering.com>
2024-12-06 15:18:31 +0100

    test(e2e): add test cases to dashboard setsList e2e tests
    Refs: projects#1359

--- e2e/tests/pages/dashboard.setsList.test.ts
+++ e2e/tests/pages/dashboard.setsList.test.ts
@@ -2,6 +2,66 @@ import tipCards from '@e2e/lib/tipCards'
 import tipCardsApi from '@e2e/lib/tipCardsApi'
 
 describe('Dashboard Sets List', () => {
+  it('shows a message when the user is logged out', () => {
+    tipCards.gotoDashboardPage()
+
+    cy.getTestElement('sets-list-message-not-logged-in').should('exist')
+    cy.getTestElement('modal-login').should('not.exist')
+  })
+
+  it('should open the modal login', () => {
+    tipCards.gotoDashboardPage()
+
+    cy.getTestElement('sets-list-message-not-logged-in').then(($el) => {
+      cy.wrap($el.find('button')).click()
+    })
+
+    cy.getTestElement('modal-login').should('exist')
+  })
+
+  it('loads 0 sets and displays a message when the user is logged in', () => {
+    tipCardsApi.auth.login()
+
+    tipCards.gotoDashboardPage()
+
+    cy.getTestElement('sets-list-message-empty').should('exist')
+  })
+
+  it('should login and display the user\'s sets aferwards', () => {
+    // preparation: create a user and 100 sets w/o logging in
+    cy.task<{ publicKeyAsHex: string, privateKeyAsHex: string }>('lnurl:createRandomKeyPair').then((keyPair) => {
+      cy.wrap(keyPair).as('keyPair')
+      cy.task<{ userId: string, lnurlAuthKey: string }>('db:createUser', { lnurlAuthKey: keyPair.publicKeyAsHex }).then(({ userId }) => {
+        cy.log('userId', userId)
+        cy.task('db:create100TestSets', { userId })
+      })
+    })
+    tipCards.gotoDashboardPage()
+
+    // log in via ui
+    cy.getTestElement('sets-list-message-not-logged-in').then(($el) => {
+      cy.wrap($el.find('button')).click()
+    })
+    cy.getTestElement('lightning-qr-code-image').then(($el) => {
+      const lnurlAuthUrl = $el.attr('href').substring(10)
+      cy.wrap(lnurlAuthUrl).as('lnurlAuthUrl')
+    })
+    tipCardsApi.auth.lnurlAuthLoginWithWrappedKeyPair()
+    cy.getTestElement('modal-login-close-button').click()
+
+    // the sets should get loaded
+    cy.get('[data-test=sets-list] [data-test=sets-list-item]').should('have.length', 3)
+  })
+
+  it('loads a single set', () => {
+    tipCardsApi.auth.login()
+    tipCardsApi.set.generateAndAddSet()
+
+    tipCards.gotoDashboardPage()
+
+    cy.get('[data-test=sets-list] [data-test=sets-list-item]').should('have.length', 1)
+  })
+
   it('loads 100 sets and displays the 3 most recently changed (descending)', () => {
     tipCardsApi.auth.login()
     cy.get('@userId').then((userId) => {
