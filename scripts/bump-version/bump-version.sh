f5c0b6301dcb4cd96a5e02c2327a47eff1629859
Philipp Horwath <fil@satoshiengineering.com>
2024-12-18 09:20:51 +0100

    chore: add changelog to tag message
    Refs: projects#1379

--- scripts/bump-version/bump-version.sh
+++ scripts/bump-version/bump-version.sh
@@ -12,9 +12,10 @@ git commit -m "chore(release): $NEXT_VERSION" --no-verify
 
 # create tag for changelogn
 git tag $NEXT_VERSION
+CHANGELOG=$(npx changelogen --from $CURRENT_VERSION --to $NEXT_VERSION)
 npx changelogen --from $CURRENT_VERSION --to $NEXT_VERSION --output CHANGELOG.md
 git add CHANGELOG.md
 git commit --amend --no-edit --no-verify
 
 # Force update the tag to point to the amended commit
-git tag -f $NEXT_VERSION
+git tag -f $NEXT_VERSION -m "$CHANGELOG"
