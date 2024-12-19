1b3b935bbafdc264b8e1e1e1d93e229c55055583
Thomas Schagerl <tom@satoshiengineering.com>
2024-12-19 18:21:52 +0100

    refactor(e2e): reduce sets for a dashboard setsList test
    Refs: projects#1423

--- e2e/tests/pages/dashboard.setsList.test.ts
+++ e2e/tests/pages/dashboard.setsList.test.ts
@@ -27,20 +27,24 @@ describe('Dashboard Sets List', () => {
     cy.getTestElement('sets-list-message-empty').should('exist')
   })
 
-  it.skip('should login and display the user\'s sets aferwards', () => {
-    // preparation: create a user and 100 sets w/o logging in
+  it('should login and display the user\'s sets aferwards', () => {
+    // preparation: create a user and a set w/o logging in
     cy.task<{ publicKeyAsHex: string, privateKeyAsHex: string }>('lnurl:createRandomKeyPair').then((keyPair) => {
       cy.wrap(keyPair).as('keyPair')
       cy.task<{ userId: string, lnurlAuthKey: string }>('db:createUser', { lnurlAuthKey: keyPair.publicKeyAsHex }).then(({ userId }) => {
-        cy.task('db:create100TestSets', { userId })
+        cy.task('db:createSetsWithSetFunding', {
+          userId,
+          numberOfSets: 5,
+          numberOfCardsPerSet: 8,
+        }).then((sets) => {
+          cy.wrap(sets).as('sets')
+        })
       })
     })
     tipCards.gotoDashboardPage()
 
     // log in via ui
-    cy.getTestElement('sets-list-message-not-logged-in').then(($el) => {
-      cy.wrap($el.find('button')).click()
-    })
+    cy.get('[data-test=sets-list-message-not-logged-in] button').click()
     cy.getTestElement('lightning-qr-code-image').then(($el) => {
       const lnurlAuthUrl = $el.attr('href').substring(10)
       cy.wrap(lnurlAuthUrl).as('lnurlAuthUrl')
