00e3edc2492f67e10c895bc0e1796671adf56667
Thomas Schagerl <tom@satoshiengineering.com>
2024-12-22 17:33:43 +0100

    refactor(backend): clean code OpenBulkWithdrawTaskBuilder
    Refs: projects#1360

--- backend/src/domain/OpenBulkWithdrawTaskBuilder.ts
+++ backend/src/domain/OpenBulkWithdrawTaskBuilder.ts
@@ -1,12 +1,13 @@
 import { CardStatusEnum } from '@shared/data/trpc/CardStatusDto.js'
 
 import { asTransaction } from '@backend/database/client.js'
-import { LnurlW } from '@backend/database/schema/LnurlW.js'
+import type { LnurlW } from '@backend/database/schema/LnurlW.js'
 
 import CardStatusCollection from './CardStatusCollection.js'
 import CardStatus from './CardStatus.js'
 import { OpenBulkwithdrawTask } from './OpenBulkWithdrawTask.js'
 import CardVersionWithInvoices from '@backend/database/data/CardVersionWithInvoices.js'
+import type { SetWithSettings } from '@backend/database/data/SetWithSettings.js'
 
 export default class OpenTaskBuilder {
   public readonly cardStatusCollection: CardStatusCollection
@@ -16,37 +17,9 @@ export default class OpenTaskBuilder {
   }
 
   public async build(): Promise<void> {
-    const bulkWithdrawCardStatuses = this.cardStatusCollection.data
-      .filter((cardStatus) => cardStatus.status === CardStatusEnum.enum.isLockedByBulkWithdraw)
-    const openTasksByLnurlW = bulkWithdrawCardStatuses.reduce<Record<string, { cardStatuses: CardStatus[], lnurlW: LnurlW }>>((acc, cardStatus) => {
-      const lnurlW = cardStatus.lnurlW
-      if (lnurlW == null) {
-        return acc
-      }
-      if (acc[lnurlW.lnbitsId] == null) {
-        acc[lnurlW.lnbitsId] = {
-          cardStatuses: [],
-          lnurlW,
-        }
-      }
-      acc[lnurlW.lnbitsId].cardStatuses.push(cardStatus)
-      return acc
-    }, {})
-    const setWithSettingsByLnurlW = await asTransaction(async (queries) => queries.getSetWithSettingsByLnurlW(Object.keys(openTasksByLnurlW)))
-    this.openBulkWithdrawTasks = Object.values(openTasksByLnurlW)
-      .map(({ cardStatuses, lnurlW }) => {
-        const setWithSettings = setWithSettingsByLnurlW[lnurlW.lnbitsId]
-        return OpenBulkwithdrawTask.fromData({
-          set: {
-            id: setWithSettings.id,
-            created: setWithSettings.created,
-            changed: setWithSettings.changed,
-          },
-          setSettings: setWithSettings.settings,
-          cards: cardStatuses.map((cardStatus) => new CardVersionWithInvoices(cardStatus.cardVersion, cardStatus.invoices)),
-          lnurlW,
-        })
-      })
+    this.groupCardStatusesByLnurlW()
+    await this.loadSetInfoForLnurlWs()
+    this.openBulkWithdrawTasks = this.lnurlWs.map((lnurlW) => this.openBulkWithdrawTaskFromLnurlW(lnurlW))
   }
 
   public get openTasks(): OpenBulkwithdrawTask[] {
@@ -54,4 +27,62 @@ export default class OpenTaskBuilder {
   }
 
   private openBulkWithdrawTasks: OpenBulkwithdrawTask[] = []
+  private cardStatusesByLnurlW: Record<LnurlW['lnbitsId'], CardStatus[]> = {}
+  private lnurlWs: LnurlW[] = []
+  private setWithSettingsByLnurlW: Record<LnurlW['lnbitsId'], SetWithSettings> = {}
+
+  private groupCardStatusesByLnurlW(): void {
+    this.bulkWithdrawCardStatuses.forEach((cardStatus) => {
+      const { lnurlW } = cardStatus
+      if (lnurlW == null) {
+        return
+      }
+      this.addCardStatus(lnurlW.lnbitsId, cardStatus)
+      this.addLnurlW(lnurlW)
+    })
+  }
+
+  private addCardStatus(lnbitsId: LnurlW['lnbitsId'], cardStatus: CardStatus): void {
+    this.makeSureCardStatusArrayExists(lnbitsId)
+    this.cardStatusesByLnurlW[lnbitsId].push(cardStatus)
+  }
+
+  private makeSureCardStatusArrayExists(lnbitsId: LnurlW['lnbitsId']): void {
+    if (this.cardStatusesByLnurlW[lnbitsId] == null) {
+      this.cardStatusesByLnurlW[lnbitsId] = []
+    }
+  }
+
+  private addLnurlW(lnurlW: LnurlW): void {
+    if (this.isNewLnurlW(lnurlW)) {
+      this.lnurlWs.push(lnurlW)
+    }
+  }
+
+  private isNewLnurlW(lnurlW: LnurlW): boolean {
+    return this.lnurlWs.find((existingLnurlW) => existingLnurlW.lnbitsId === lnurlW.lnbitsId) == null
+  }
+
+  private get bulkWithdrawCardStatuses(): CardStatus[] {
+    return this.cardStatusCollection.data
+      .filter((cardStatus) => cardStatus.status === CardStatusEnum.enum.isLockedByBulkWithdraw)
+  }
+
+  private async loadSetInfoForLnurlWs(): Promise<void> {
+    this.setWithSettingsByLnurlW = await asTransaction(async (queries) => queries.getSetWithSettingsByLnurlW(this.lnurlWs.map((lnurlW) => lnurlW.lnbitsId)))
+  }
+
+  private openBulkWithdrawTaskFromLnurlW(lnurlW: LnurlW): OpenBulkwithdrawTask {
+    const setWithSettings = this.setWithSettingsByLnurlW[lnurlW.lnbitsId]
+    return OpenBulkwithdrawTask.fromData({
+      set: {
+        id: setWithSettings.id,
+        created: setWithSettings.created,
+        changed: setWithSettings.changed,
+      },
+      setSettings: setWithSettings.settings,
+      cards: this.cardStatusesByLnurlW[lnurlW.lnbitsId].map((cardStatus) => new CardVersionWithInvoices(cardStatus.cardVersion, cardStatus.invoices)),
+      lnurlW,
+    })
+  }
 }
