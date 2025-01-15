0838b1553acff2ec47faac90e12995a6c46ca9c7
Thomas Schagerl <tom@satoshiengineering.com>
2025-01-15 09:08:06 +0100

    refactor(e2e): apply youtube error handling earlier
    Refs: projects#1425

--- e2e/lib/pages/home.ts
+++ e2e/lib/pages/home.ts
@@ -1,8 +1,8 @@
 import { TIPCARDS_ORIGIN } from '@e2e/lib/constants'
 import { gotoPage } from './utils'
 
-const LOGIN_PAGE_URL = new URL('/', TIPCARDS_ORIGIN)
+const HOME_PAGE_URL = new URL('/', TIPCARDS_ORIGIN)
 
 export const goto = () => {
-  gotoPage(LOGIN_PAGE_URL)
+  gotoPage(HOME_PAGE_URL)
 }
--- e2e/tests/features/homePageLinks.test.ts
+++ e2e/tests/features/homePageLinks.test.ts
@@ -1,39 +1,46 @@
-import { TIPCARDS_ORIGIN } from '@e2e/lib/constants'
-
-const HOME_PAGE_URL = new URL('/', TIPCARDS_ORIGIN)
+import tipCards from '@e2e/lib/tipCards'
 
 describe('homePageLinks', () => {
+  beforeEach(() => {
+    // we don't care about errors on youtube
+    cy.origin('https://www.youtube.com', () => {
+      cy.on('uncaught:exception', () => false) // this makes sure the test won't fail
+    })
+  })
+
   it('should navigate to the cards page when the create button is clicked', () => {
-    cy.visit(HOME_PAGE_URL.href)
+    tipCards.home.goto()
+
     cy.get('[data-test="hero-section"] [data-test="button-create"]').should('exist').click()
+
     cy.url().should('contain', 'cards')
   })
 
   it('should navigate to the dashboard page when the dashboard button is clicked', () => {
-    cy.visit(HOME_PAGE_URL.href)
+    tipCards.home.goto()
+
     cy.get('[data-test="hero-section"] [data-test="button-dashboard"]').should('exist').click()
+
     cy.url().should('contain', '/dashboard')
   })
 
   it('should render the two expected sliders', () => {
-    cy.visit(HOME_PAGE_URL.href)
+    tipCards.home.goto()
+
     cy.get('[data-test="slider-how-it-works"]').should('exist')
     cy.get('[data-test="slider-video-guides"]').should('exist')
   })
 
   it('should navigate to the cards page when the button in the first slider is clicked', () => {
-    cy.visit(HOME_PAGE_URL.href)
+    tipCards.home.goto()
+
     cy.get('[data-test="slider-how-it-works"] [data-test="slider-button-start"]').first().click()
+
     cy.url().should('contain', '/cards')
   })
 
-  it.skip('should navigate to youtube when the play button in the second slider is clicked', () => {
-    cy.visit(HOME_PAGE_URL.href)
-
-    // we don't care about error on youtube
-    cy.origin('https://www.youtube.com', () => {
-      cy.on('uncaught:exception', () => false) // this makes sure the test won't fail
-    })
+  it.only('should navigate to youtube when the play button in the second slider is clicked', () => {
+    tipCards.home.goto()
 
     cy.get('[data-test="slider-video-guides"] [data-test="slider-video-link"]').eq(1)
       .should('exist')
