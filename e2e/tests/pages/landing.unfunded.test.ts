d7e3318578ca9d146a623da569d0f4d2af51f9af
dr-erych <dave@satoshiengineering.com>
2025-01-29 17:56:36 +0100

    fix(e2e): typo in function name
    Refs: projects#1486

--- e2e/tests/pages/landing.unfunded.test.ts
+++ e2e/tests/pages/landing.unfunded.test.ts
@@ -1,4 +1,4 @@
-import { caluclateFeeForCard } from '../../../shared/src/modules/feeCalculation'
+import { calculateFeeForCard } from '../../../shared/src/modules/feeCalculation'
 
 import { generateCardHash, generateCardHashForSet } from '@e2e/lib/api/data/card'
 import { generateSetId } from '@e2e/lib/api/data/set'
@@ -18,7 +18,7 @@ describe('Landing Page', () => {
   it('should redirect to the funding page, if an unpaid invoice exists', () => {
     generateCardHash().then((cardHash) => {
       tipCardsApi.card.createInvoiceForCardHash(cardHash, 210)
-      const totalBtcInclFee = (210 + caluclateFeeForCard(210)) / 100_000_000
+      const totalBtcInclFee = (210 + calculateFeeForCard(210)) / 100_000_000
 
       tipCards.landing.gotoPreview(cardHash)
 
