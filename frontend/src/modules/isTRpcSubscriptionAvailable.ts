f51c855b894a16d325d7e3523e4e804406a50d8f
Thomas Schagerl <tom@satoshiengineering.com>
2025-02-28 09:15:57 +0100

    fix: turning on trpc subscriptions again
    Refs: projects#1521

--- frontend/src/modules/isTRpcSubscriptionAvailable.ts
+++ frontend/src/modules/isTRpcSubscriptionAvailable.ts
@@ -2,5 +2,7 @@ export default () => {
   // trpc subscriptions are currently making problems with safari on ios
   // maybe we will add a browser detection here in the future
   // https://gitlab.satoshiengineering.com/satoshiengineering/projects/-/issues/1521#note_22890
-  return false
+
+  // 2025-02-28 ios safari polling is not working currently, so we use subscriptions again
+  return true
 }
