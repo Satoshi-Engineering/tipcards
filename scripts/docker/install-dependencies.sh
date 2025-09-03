48c372660c5716d23eec0e33ba06d47a39234f5c
Philipp Horwath <fil@satoshiengineering.com>
2025-09-03 14:34:33 +0200

    chore: add system architecture to install dependencies script
    Refs: projects#1916

--- scripts/docker/install-dependencies.sh
+++ scripts/docker/install-dependencies.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+SYSTEM_ARCHITECTURE=$(uname -m)
+NODE_MODULES_DIR=$(pwd)/node_modules
+
+if [ "$SYSTEM_ARCHITECTURE" = "arm64" ]; then
+  NODE_MODULES_DIR=$(pwd)/node_modules_docker
+fi
+
 docker run --rm \
   -v $(pwd):/app \
-  -v $(pwd)/node_modules_docker:/app/node_modules \
+  -v $NODE_MODULES_DIR:/app/node_modules \
   -w /app \
   node:lts-bookworm-slim /bin/bash \
   -c "npm i"
