656f6bc8bde834495c36f13b86864017daffccbc
dr-erych <dave@satoshiengineering.com>
2025-11-20 11:44:46 +0100

    fix: check attribute content in login modal qr code to be even safer
    Refs: projects#2190

--- e2e-playwright/utils/auth/login.ts
+++ e2e-playwright/utils/auth/login.ts
@@ -4,7 +4,10 @@ import { lnurlAuth } from '../lnbits/api/lnurlAuth'
 export const loginViaUi = async ({ page, lnbitsApiContext }) => {
   await page.locator('[data-test="the-header-main-nav-button"]').click()
   await page.locator('[data-test="main-nav-link-login"]').click()
-  await expect(page.locator('[data-test="lightning-qr-code-image"]')).toHaveAttribute('href', /\\S+/)
+  await expect(page.locator('[data-test="lightning-qr-code-image"]')).toHaveAttribute(
+    'href',
+    /^lightning:lnurl1[qpzry9x8gf2tvdw0s3jn54khce6mua7l]+$/i, // LNURL bech32 encoded string
+  )
   const lnurl = await page.locator('[data-test="lightning-qr-code-image"]').getAttribute('href')
   if (!lnurl) {
     throw new Error('LNURL auth link not found or empty')
