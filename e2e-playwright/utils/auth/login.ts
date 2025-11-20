0aff37651aff1c394e633327248f7ab7d278592a
dr-erych <dave@satoshiengineering.com>
2025-11-20 11:27:47 +0100

    fix: prevent test flakiness by checking for attribute existance before reading it also in login
    Refs: projects#2190

--- e2e-playwright/utils/auth/login.ts
+++ e2e-playwright/utils/auth/login.ts
@@ -4,9 +4,11 @@ import { lnurlAuth } from '../lnbits/api/lnurlAuth'
 export const loginViaUi = async ({ page, lnbitsApiContext }) => {
   await page.locator('[data-test="the-header-main-nav-button"]').click()
   await page.locator('[data-test="main-nav-link-login"]').click()
-  await expect(page.locator('[data-test="lightning-qr-code-image"]')).toBeVisible()
+  await expect(page.locator('[data-test="lightning-qr-code-image"]')).toHaveAttribute('href', /\\S+/)
   const lnurl = await page.locator('[data-test="lightning-qr-code-image"]').getAttribute('href')
-
+  if (!lnurl) {
+    throw new Error('LNURL auth link not found or empty')
+  }
   await lnurlAuth(lnbitsApiContext, lnurl)
 
   await expect(page.locator('[data-test="lightning-qr-code-image-success"]')).toBeVisible({ timeout: 60000 })
