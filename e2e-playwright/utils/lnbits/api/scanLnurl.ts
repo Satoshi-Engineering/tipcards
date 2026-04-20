f4222796dd073f76fe2f3324c7fcf30509dfacb5
dr-erych <dave@satoshiengineering.com>
2026-04-20 22:23:32 +0200

    fix(e2e): use POST body for LNbits lnurlscan API

--- e2e-playwright/utils/lnbits/api/scanLnurl.ts
+++ e2e-playwright/utils/lnbits/api/scanLnurl.ts
@@ -2,7 +2,11 @@ import { APIRequestContext } from '@playwright/test'
 import { removeLightningPrefix } from '@e2e-playwright/utils/removeLightningPrefix'
 
 export default async (context: APIRequestContext, lnurl: string) => {
-  const response = await context.get(`/api/v1/lnurlscan/${removeLightningPrefix(lnurl)}`)
+  const response = await context.post('/api/v1/lnurlscan', {
+    data: {
+      lnurl: removeLightningPrefix(lnurl),
+    },
+  })
   if (!response.ok()) {
     throw new Error(`Failed to scan LNURL: ${await response.text()}`)
   }
